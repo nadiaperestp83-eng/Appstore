df2d25a66ea16c4701035ec1f49cdbaca31eff35
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-25 12:16:05 -0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:playstore_flutter/main.dart';
+import 'package:playstore/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
