7c70ad752a900b37f9bcdeb49a7184570ab5ae3b
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-25 09:36:08 -0300

    Update isar_cache_service.dart

--- lib/cache/isar_cache_service.dart
+++ lib/cache/isar_cache_service.dart
@@ -1,4 +1,4 @@
-import 'package:isar/isar.dart';
+import 'package:isar_community/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'cached_store_app.dart';
 import '../models/store_app.dart';
