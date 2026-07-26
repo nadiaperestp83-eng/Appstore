08c11c8d6303c6e321f2907df6f9f188e151b608
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-26 06:35:45 -0300

    Update PSMTopSellingFragment.dart

--- lib/components/PSMTopSellingFragment.dart
+++ lib/components/PSMTopSellingFragment.dart
@@ -3,6 +3,7 @@ import 'package:nb_utils/nb_utils.dart';
 import 'package:playstore_flutter/model/PSAppbarModel.dart';
 import 'package:playstore_flutter/screens/PSBooKDetailsScreen.dart';
 import 'package:playstore_flutter/utils/AppWidget.dart';
+import 'package:playstore_flutter/screens/PSBooKDetailsScreen.dart';
 
 class PSMTopSellingFragment extends StatefulWidget {
   static String tag = '/MTopSelling';
