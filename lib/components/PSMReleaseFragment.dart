bee947f78b58b82bab081636ad7a5e099482bf04
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-26 06:34:36 -0300

    Update PSMReleaseFragment.dart

--- lib/components/PSMReleaseFragment.dart
+++ lib/components/PSMReleaseFragment.dart
@@ -3,6 +3,7 @@ import 'package:nb_utils/nb_utils.dart';
 import 'package:playstore_flutter/model/PSAppbarModel.dart';
 import 'package:playstore_flutter/screens/PSBooKDetailsScreen.dart';
 import 'package:playstore_flutter/utils/AppWidget.dart';
+import 'package:playstore_flutter/screens/PSBooKDetailsScreen.dart';
 
 class PSMReleaseFragment extends StatefulWidget {
   static String tag = '/MnewReleases';
