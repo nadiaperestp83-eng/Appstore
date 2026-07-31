30fcbf2ecc0b5e8ba275e40d4aa19b16adaae2ba
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-07-31 07:53:20 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
     return Observer(
       builder: (_) {
         return MaterialApp(
-          title: 'PlayStore flutter',
+          title: 'Zircon',
           theme: !appStore.isDarkModeOn ? AppThemeData.lightTheme : AppThemeData.darkTheme,
           builder: scrollBehaviour(),
           debugShowCheckedModeBanner: false,
